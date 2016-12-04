f6b34be13d579eb17172971b53381ee60b9b1752
Marius Petcu <dapetcu21@gmail.com>
2016-12-05 00:23:09 +0200

    Improve list item coloring

--- src/components/IssueListItem.js
+++ src/components/IssueListItem.js
@@ -2,24 +2,22 @@ import React, { Component } from 'react'
 
 import styles from './IssueListItem.scss'
 
-export default class IssueListItem extends Component {
-
-  getLabelColor = (status) => {
-    switch (status) {
-      case 1:
-        return styles.blue
-      case 2:
-        return styles.yellow
-      default:
-        return styles.red
-    }
+const getLabelColor = (status) => {
+  switch (status) {
+    case 1:
+      return styles.open
+    case 2:
+      return styles.inProgress
+    default:
+      return styles.closed
   }
+}
+
+export default class IssueListItem extends Component {
 
   render () {
     const { issue } = this.props
-    // return <div>{JSON.stringify(issue)}</div>
-    return <div className={styles.box}>
-      <span className={styles.status + ' ' + this.getLabelColor(issue.status)} />
+    return <div className={styles.box + ' ' + getLabelColor(issue.status)}>
       <a href={'/issue/' + issue.id}>{'[' + issue.issue_code + ']' + ' - ' + '[' + issue.name + ']'}</a>
     </div>
   }
