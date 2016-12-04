66be92426773c9f3bbaa2eab1b8009ea36c9cbd8
CristiD <cristian.dragu1406@gmail.com>
2016-12-04 23:34:50 +0200

    Added issue list item impl

--- src/components/IssueListItem.js
+++ src/components/IssueListItem.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react'
 
+import styles from './IssueListItem.scss'
+
 export default class IssueListItem extends Component {
+
+  getLabelColor = (status) => {
+    switch (status) {
+      case 1:
+        return styles.blue
+      case 2:
+        return styles.yellow
+      default:
+        return styles.red
+    }
+  }
+
   render () {
     const { issue } = this.props
-    return <div>{JSON.stringify(issue)}</div>
+    console.log(issue)
+    // return <div>{JSON.stringify(issue)}</div>
+    return <div className={styles.box}>
+      <span className={styles.status + ' ' + this.getLabelColor(issue.status)} />
+      <a href={'/issue/' + issue.id}>{'[' + issue.issue_code + ']' + ' - ' + '[' + issue.name + ']'}</a>
+    </div>
   }
 }
